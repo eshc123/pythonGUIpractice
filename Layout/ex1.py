e43d2c42c82968b2280fb8f992a34d43ab086157
eshc123 <eshc@naver.com>
2020-06-29 10:13:27 +0900

    layout test1

---
+++ Layout/ex1.py
@@ -0,0 +1,34 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton
+
+
+class MyApp(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        label1 = QLabel('Label1', self)
+        label1.move(20, 20) # 라벨을 만들고 20,20에 위치함, 좌표계는 왼쪽 상단부터 시작해서 x축은 오른쪽으로 y축을 아래로 향한다.
+        label2 = QLabel('Label2', self)
+        label2.move(20, 60)
+
+        btn1 = QPushButton('Button1', self)
+        btn1.move(80, 13)
+        btn2 = QPushButton('Button2', self)
+        btn2.move(80, 53)
+
+        #응용
+        btn3 = QPushButton('테스트용', self)
+        btn3.move(80, 93)
+
+        self.setWindowTitle('Absolute Positioning')
+        self.setGeometry(300, 300, 400, 200)
+        self.show()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    sys.exit(app.exec_())
